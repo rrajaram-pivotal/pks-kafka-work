dd7d954cc93f00870f88cd8d43164ff4e0694846
rrajaram-pivotal <rrajaram@pivotal.io>
2019-07-09 15:24:50 -0400

    Changes to refresh page every 30 seconds

--- queryorder/src/main/resources/static/queryorder.js
+++ queryorder/src/main/resources/static/queryorder.js
@@ -1,8 +1,22 @@
-angular.module('queryorderdemo', [])
-.controller('QueryOrder', function($scope, $http) {
-    $http.get('http://localhost:8085/v1/queryorder/').
-        then(function(response) {
-            $scope.orderlist = response.data;
-            
-        });
+/*angular.module('queryorderdemo', [])
+.controller('QueryOrder', function($scope, $interval,$http) {
+	
+	$interval(function ()
+			alert("In");
+			$http.get('http://localhost:8085/v1/queryorder/').
+			then(function(response) {
+				$scope.orderlist = response.data;
+				
+			}), 5000);
+});*/
+
+angular.module('queryorderdemo',[]).controller("QueryOrder", function ($scope,$http,$interval){
+    $scope.reload = function () {
+        $http.get('http://queryorder:8085/v1/queryorder/').
+            then(function (response) {
+            	$scope.orderlist = response.data;
+            });
+    };
+    $scope.reload();
+    $interval($scope.reload, 10000);
 });
